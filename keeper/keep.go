a1f9cfdd74236265272e267c3f2b18bacd129451
Pavlo <malovanniy@gmail.com>
2022-05-15 16:23:18 +0300

    keeper + tests

---
+++ keeper/keep.go
@@ -0,0 +1,34 @@
+package keeper
+
+import "errors"
+
+type Keeper interface {
+	Get(key string) (string, error)
+	Set(key string, message string) error
+	Delete(key string)
+}
+
+type DummyKepper struct {
+	mem map[string]string
+}
+
+func (d DummyKepper) Get(key string) (string, error) {
+	v, ok := d.mem[key]
+	if !ok {
+		return "", errors.New("message not found")
+	}
+	d.Delete(key)
+	return v, nil
+}
+
+func (d DummyKepper) Set(key, message string) error {
+	if _, ok := d.mem[key]; !ok {
+		d.mem[key] = message
+		return nil
+	}
+	return errors.New("ALready exists")
+}
+
+func (d DummyKepper) Delete(key string) {
+	delete(d.mem, key)
+}
